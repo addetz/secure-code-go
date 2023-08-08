3144bbe7c8989473cdeb0724769ab2e3ebfda6aa
addetz <adelinasimion@gmail.com>
2023-08-08 14:55:52 +0100

    add demo 1 code

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # secure-code-go
-Demo repository for the talk "The shimmy to the left: why security is coming for engineers"
+Demo repository for the talk ["The shimmy to the left: why security is coming for engineers"](https://www.gophercon.co.uk/schedule/)
+
+## Environment variables
+Certificates & keys can be easily generated for local testing with [`mkcert`](https://github.com/FiloSottile/mkcert). 
+
+```bash
+export SERVER_CERT_FILE="localhost.pem"
+export SERVER_KEY_FILE="localhost-key.pem"
+export SERVER_PORT="1232"
+```
+
+## Execute demos
+Run the demo servers one by one: 
+```bash
+go run demo1/server.go
+```
---
+++ demo1/server.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	echo "github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+const TIMEOUT = 3 * time.Second
+
+type Response struct {
+	Message string `json:"message"`
+}
+
+func main() {
+	// Read paths to certificate & private key from environment variables
+	certFile, ok := os.LookupEnv("SERVER_CERT_FILE")
+	if !ok {
+		log.Fatal("SERVER_CERT_FILE variable must be set")
+	}
+	keyFile, ok := os.LookupEnv("SERVER_KEY_FILE")
+	if !ok {
+		log.Fatal("SERVER_KEY_FILE variable must be set")
+	}
+
+	// Read port if one is set
+	port := readPort()
+
+	// Initialise echo
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Configure server
+	s := http.Server{
+		Addr:              fmt.Sprintf(":%s", port),
+		Handler:           e,
+		ReadTimeout:       TIMEOUT,
+		ReadHeaderTimeout: TIMEOUT,
+		WriteTimeout:      TIMEOUT,
+		IdleTimeout:       TIMEOUT,
+	}
+
+	// Set up the root route
+	e.GET("/", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, &Response{
+			Message: "Hello, Gophers!",
+		})
+	})
+
+	log.Printf("Listening on :%s...\n", port)
+	if err := s.ListenAndServeTLS(certFile, keyFile); err != http.ErrServerClosed {
+		log.Fatal(err)
+	}
+}
+
+func readPort() string {
+	port, ok := os.LookupEnv("SERVER_PORT")
+	if !ok {
+		return "1232"
+	}
+	return port
+}
